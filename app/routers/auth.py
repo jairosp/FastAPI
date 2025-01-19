8ba659a49250d3b1fc4165dcc05def747ce92d10
Jairo <jairosp2000@gmail.com>
2025-01-19 17:09:45 +0100

    Few changes and testing

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -6,7 +6,7 @@ from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=["Authentication"])
 
-@router.get("/login", response_model=schemas.Token)
+@router.post("/login", response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends() ,db: Session = Depends(database.get_db)):
     statement = select(models.User).filter(models.User.email == user_credentials.username)
     results = db.exec(statement)
@@ -16,7 +16,7 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends() ,db: Session =
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail= "invalid credentials")
     
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid credentials")
     
     # Create a token
     access_token = oauth2.create_access_token(data = {"user_id": user.id})
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,22 +1,39 @@
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
+from jose import jwt
+from .database import client, session
+import pytest
 from app.config import settings
 
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "he@email.com", "password": "password123"}
+    res = client.post(
+        "/users/", json=user_data)
+    
+    new_user = res.json()
+    new_user['password'] = user_data['password']
 
+    assert res.status_code == 201
 
-client = TestClient(app)
+    return new_user
 
-def test_root():
-    res = client.get("/")
-    assert res.json().get('message') == "Hello World!!!!!!!!"
-    assert res.status_code == 200
-
-def test_create_user():
+def test_create_user(client):
     res = client.post(
         "/users/", json={"email": "he@gmail.com", "password": "password123"})
     
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "he@gmail.com"
     assert res.status_code == 201
+
+def test_login_user(client, test_user):
+    res = client.post(
+        "/login", data={"username": test_user['email'], "password": test_user['password']}
+    )
+    login_res = schemas.Token(**res.json())
+
+    payload = jwt.decode(login_res.access_token , settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get('user_id')
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
     
